251205cf65d4a681524b0d8582fa2ff0dc2743be
Flohest <florian.goermann@gmail.com>
2024-07-18 11:59:53 +0200

    [RTC Minimalcode für Aufgabe 2]

---
+++ RTC/src/main.cpp
@@ -0,0 +1,42 @@
+#include <RTClib.h>
+#include <SPI.h>
+
+RTC_DS1307 rtc;
+
+char daysOfTheWeek[7][12] = {
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+};
+
+void setup () {
+  Serial.begin(9600);
+
+  // automatically sets the RTC to the date & time on PC this sketch was compiled
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+
+}
+
+void loop () {
+  DateTime now = rtc.now();
+  Serial.print("Date & Time: ");
+  Serial.print(now.year(), DEC);
+  Serial.print('/');
+  Serial.print(now.month(), DEC);
+  Serial.print('/');
+  Serial.print(now.day(), DEC);
+  Serial.print(" (");
+  Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+  Serial.print(") ");
+  Serial.print(now.hour(), DEC);
+  Serial.print(':');
+  Serial.print(now.minute(), DEC);
+  Serial.print(':');
+  Serial.println(now.second(), DEC);
+
+  delay(1000); 
+}
