cf75772be4150c89e928341f544e2738d2710f5f
MaxWe18 <maxweber-8@t-online.de>
2024-07-18 12:48:42 +0200

    Beispiel für Aufgabe 3 (V. 1)

--- Minimalbeispiel/src/main.cpp
+++ Minimalbeispiel/src/main.cpp
@@ -2,23 +2,49 @@
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME680.h>
+#include <RTClib.h>
+#include <SPI.h>
+#include "LCD_Driver.h"
+#include "GUI_Paint.h"
 
 #define SEALEVELPRESSURE_HPA (1013.25)
 
+char daysOfTheWeek[7][12] = {
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+};
+
 Adafruit_BME680 bme; // I2C
+RTC_DS1307 rtc;
 
 void setup() {
   Serial.begin(9600);
-  bme.begin();
 
+  bme.begin();
   bme.setTemperatureOversampling(BME680_OS_8X);
   bme.setHumidityOversampling(BME680_OS_2X);
   bme.setPressureOversampling(BME680_OS_4X);
   bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
   bme.setGasHeater(320, 150); 
+
+  // automatically sets the RTC to the date & time on PC this sketch was compiled
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+
+  // Display
+  Config_Init();
+  LCD_Init();
+  LCD_SetBacklight(100);
 }
 
+
 void loop() {
+
+  // BME680 komb. Sensor
   Serial.print(F("Temperatur = "));
   Serial.print(bme.temperature);
   Serial.println(F(" *C"));
@@ -40,5 +66,33 @@ void loop() {
   Serial.println(F(" m"));
 
   Serial.println();
-  delay(10000);
+
+  // RTC 
+  DateTime now = rtc.now();
+  Serial.print("Date & Time: ");
+  Serial.print(now.year(), DEC);
+  Serial.print('/');
+  Serial.print(now.month(), DEC);
+  Serial.print('/');
+  Serial.print(now.day(), DEC);
+  Serial.print(" (");
+  Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+  Serial.print(") ");
+  Serial.print(now.hour(), DEC);
+  Serial.print(':');
+  Serial.print(now.minute(), DEC);
+  Serial.print(':');
+  Serial.println(now.second(), DEC);
+
+  
+  // Display
+  Paint_NewImage(LCD_WIDTH, LCD_HEIGHT, 90, WHITE);
+  Paint_Clear(WHITE);
+
+  Paint_DrawString_EN(30, 10, "Hallo!", &Font24, YELLOW, RED);  
+  Paint_DrawString_EN(30, 34, "Dies Test ist!", &Font24, BLUE, CYAN);
+  
+
+  delay(1000); 
+
 }
